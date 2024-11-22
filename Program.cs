1614ef5d27d270fb4cc6757c5599a5e0c77d41f8
Rio <rio01092001@gmail.com>
2024-11-22 23:39:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string dobitnaKombinacija = "";
+Random random = new Random();
+for (int brojcanik = 0; brojcanik < 7; brojcanik++)
+{
+    int randomBroj = random.Next(1, 9);
+    dobitnaKombinacija += randomBroj;
+}
+
+string unioKorisnik = string.Empty; //""
+int brojPogodjenihBrojeva = 0;
+Console.WriteLine("Unesi 7 znameniki za loto: ");
+unioKorisnik = Console.ReadLine();
+for (int i = 0; i < unioKorisnik.Length; i++)
+{
+    if (unioKorisnik[i] == dobitnaKombinacija[i])
+    {
+       brojPogodjenihBrojeva++;
+    }
+
+}
+
+if (brojPogodjenihBrojeva == dobitnaKombinacija.Length)
+{
+    Console.WriteLine("Dobitak!");
+}
+else
+{
+    Console.WriteLine("Kombinacija nije dobitna!");
+}
